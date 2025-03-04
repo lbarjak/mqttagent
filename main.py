8cca2befa914aacdee98ab6f762b790f31527980
lbarjak <barjak.laszlo@gmail.com>
2025-03-04 22:51:55 +0100

    main.py refactored

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from MqttClient import MqttClient
 from Averages import TempsFile
 from datetime import datetime
-import time
 import json
 from Dev import Dev, devs
+import time
 
 
 def load_devices():
@@ -19,41 +19,33 @@ def load_devices():
         return []
 
 
-devices = load_devices()
-topics = []
-for device in devices:
-    if device:  # Check if device is not an empty string
-        topics.append(f"zigbee2mqtt/{device}")
-        devs[device] = Dev(device)
-for dev in devs:
-    print(f"devs[{dev}].device:", devs[dev].device)
-
-temps_file = TempsFile()
-
-
 def handle_message(topic, message):
     from_device = topic.split("/")[1]
     print(f"from: {from_device}")
-    # print(f"Message content: {message}")
     print(datetime.now())
     try:
         json_message = json.loads(message)
         temp = json_message["temperature"]
-        print(f"temperature: {temp}")
         hum = json_message["humidity"]
-        print(f"humidity: {hum}")
         batt = json_message["battery"]
-        print(f"battery: {batt}")
+
+        # Update device data
         devs[from_device].temperature = temp
         devs[from_device].humidity = hum
         devs[from_device].battery = batt
+
+        # Log temperature and save to file
         temps_file.add_temp(from_device, temp)
         temps_file.save_temps()
-        devs[from_device].average = temps_file.get_average(from_device)
-        print(f"Average temperature: {temps_file.get_average(from_device)}")
-        mqtt_client.publish_message(
-            "average/" + from_device, temps_file.get_average(from_device)
-        )
+
+        # Calculate and publish average temperature
+        average_temp = temps_file.get_average(from_device)
+        devs[from_device].average = average_temp
+
+        # mqtt_client.publish_message("average/" + from_device, average_temp)
+
+        print(f"temperature: {temp}, humidity: {hum}, battery: {batt}")
+        print(f"Average temperature: {average_temp}")
         print("-" * 54)
     except json.JSONDecodeError:
         print("Error: Failed to decode JSON message.")
@@ -63,13 +55,20 @@ def handle_message(topic, message):
         print(f"Unexpected error: {e}")
 
 
+# Load devices and initialize MQTT client
+devices = load_devices()
+topics = [f"zigbee2mqtt/{device}" for device in devices if device]
+devs.update({device: Dev(device) for device in devices if device})
+for dev in devs:
+    print(f"devs[{dev}].device:", devs[dev].device)
+temps_file = TempsFile()
+
 mqtt_client = MqttClient(
     topics=topics, on_message=handle_message
 )  # Pass the callback function
 
 mqtt_client.publish_message("alert/mqttagent", "mqttagent started")
 
-# Main loop to keep the script running
 try:
     while True:
         time.sleep(1)  # Sleep to prevent high CPU usage
